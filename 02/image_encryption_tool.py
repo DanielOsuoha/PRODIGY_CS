ce0f345b9aa9e491929f11495ba6e9fe54a9831e
Daniel Osuoha <122853068+DanielOsuoha@users.noreply.github.com>
2024-06-17 00:38:48 +0000

    added the second project

--- 02/image_encryption_tool.py
+++ 02/image_encryption_tool.py
@@ -1,29 +1,46 @@
-import cv2
+def encrypt_image(image_path, key):
+    file = open(image_path, 'rb')
+    image = file.read()
+    file.close()
+    image = bytearray(image)
+    #performing XOR on the image
+    for index, value in enumerate(image):
+        image[index] = value ^ key
+    #opening file for writing purposes
+    file = open(image_path, 'wb')
+    #write the image
+    file.write(image)
+    file.close()
+    print("Encryption done...")
+    
 
-def encrypt_image(image_path):
-    # Function to encrypt an image
-    img = cv2.imread(image_path)
-    # Perform encryption operations (e.g., swapping pixels, mathematical operations)
-    # Save the encrypted image
-    cv2.imwrite('encrypted_image.png', img)
-
-def decrypt_image(image_path):
-    # Function to decrypt an image
-    img = cv2.imread(image_path)
-    # Perform decryption operations (reverse of encryption)
-    # Save the decrypted image
-    cv2.imwrite('decrypted_image.png', img)
+def decrypt_image(image_path, key):
+    file = open(image_path, 'rb')
+    image = file.read()
+    file.close()
+    image = bytearray(image)
+    
+    #performing XOR on the image
+    for index, value in enumerate(image):
+        image[index] = value ^ key
+    #opening file for writing purposes
+    file = open(image_path, 'wb')
+    #write the image
+    file.write(image)
+    file.close()
+    print("Decryption done...")
 
 def main():
     # Example of usage:
     image_path = input("Enter image file path: ")
     operation = input("Enter 'encrypt' or 'decrypt': ").lower()
-
+    key = int(input("Enter key (a number you need to remember): ").lower())
+    
     if operation == 'encrypt':
-        encrypt_image(image_path)
+        encrypt_image(image_path, key)
         print("Image encrypted and saved as 'encrypted_image.png'")
     elif operation == 'decrypt':
-        decrypt_image(image_path)
+        decrypt_image(image_path, key)
         print("Image decrypted and saved as 'decrypted_image.png'")
     else:
         print("Invalid operation. Please enter 'encrypt' or 'decrypt'.")
